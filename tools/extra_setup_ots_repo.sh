5b0ac0b078db3a166305539a889a556c2da49725
Ben Hawks <bhawks@fnal.gov>
2017-09-26 10:55:30 -0500

    Updated install scripts, now creates seperate data directories for user data that's in use (NoGitData and NoGitDatabases) and a .gitignore file to ignore them and other related files

--- tools/extra_setup_ots_repo.sh
+++ tools/extra_setup_ots_repo.sh
@@ -1,3 +1,8 @@
-export USER_DATA="/home/otsdaq/Desktop/otsdaq-v1_01_01/srcs/otsdaq_prepmodernization/Data" 
-export ARTDAQ_DATABASE_URI="filesystemdb:///home/otsdaq/Desktop/otsdaq-v1_01_01/srcs/otsdaq_prepmodernization/Databases" 
+export USER_DATA="/home/otsdaq/Desktop/otsdaq-v1_01_01/srcs/otsdaq_prepmodernization/NoGitData" 
+export ARTDAQ_DATABASE_URI="filesystemdb:///home/otsdaq/Desktop/otsdaq-v1_01_01/srcs/otsdaq_prepmodernization/NoGitDatabases" 
 export USER_WEB_PATH="/home/otsdaq/Desktop/otsdaq-v1_01_01/srcs/otsdaq_prepmodernization/UserWebGUI"
+
+echo "Your User Data Path is now USER_DATA = ${USER_DATA}"
+echo "Your Database URI is now ARTDAQ_DATABASE_URI = ${ARTDAQ_DATABASE_URI}"
+echo "Your User Web Path is now USER_WEB_PATH = ${USER_WEB_PATH}"
+echo "(setup via prepmodernization)"
--- tools/install_ots_repo.sh
+++ tools/install_ots_repo.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 #Install Prepmodernization into otsdaq
+
+if [ ! -e /home/otsdaq/Desktop/otsdaq-v1_01_01/srcs/otsdaq_prepmodernization/UserWebGUI ]; then #if it's installing for the first time
+#create a symlink for our webapp
 ln -sfn /home/otsdaq/Desktop/otsdaq-v1_01_01/srcs/otsdaq_prepmodernization/UserWebGUI /home/otsdaq/Desktop/otsdaq-v1_01_01/srcs/otsdaq_utilities/WebGUI/UserWebPath
 
+#setup data folders for user data and copy example data into them
+mkdir ${MRB_SOURCE}/otsdaq_prepmodernization/NoGitData
+mkdir ${MRB_SOURCE}/otsdaq_prepmodernization/NoGitDatabases
+cp -r ${MRB_SOURCE}/otsdaq_prepmodernization/Data/* ${MRB_SOURCE}/otsdaq_prepmodernization/NoGitData/
+cp -r ${MRB_SOURCE}/otsdaq_prepmodernization/Databases/* ${MRB_SOURCE}/otsdaq_prepmodernization/NoGitDatabases/
+
+#modify setup_ots.sh
 cat setup_ots.sh > setup_ots_repo_temp.sh 
 echo >> setup_ots_repo_temp.sh 
-echo "./${MRB_SOURCE}/otsdaq_prepmodernization/tools/extra_setup_ots_repo.sh" >> setup_ots_repo_temp.sh
+echo "source ${MRB_SOURCE}/otsdaq_prepmodernization/tools/extra_setup_ots_repo.sh" >> setup_ots_repo_temp.sh
 cp setup_ots.sh setup_ots.sh.bk
 mv setup_ots_repo_temp.sh setup_ots.sh
+
+#create .gitignore to ignore user data
+touch ${MRB_SOURCE}/otsdaq_prepmodernization/.gitignore
+echo ".gitignore" >> ${MRB_SOURCE}/otsdaq_prepmodernization/.gitignore
+echo "NoGitData/" >> ${MRB_SOURCE}/otsdaq_prepmodernization/.gitignore
+echo "NoGitDatabases/" >> ${MRB_SOURCE}/otsdaq_prepmodernization/.gitignore
+echo "UserWebGUI/UserWebGUI" >> ${MRB_SOURCE}/otsdaq_prepmodernization/.gitignore
+
+
+else
+  #code to update existing installs will be here
+  echo "Nothing to do, prepmodenization already installed!"
+fi 
+
 
