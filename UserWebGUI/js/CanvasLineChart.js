6cf91c455d3eaf1b1dfc1f4d20df621b4c5d768b
Ben Hawks <bhawks@fnal.gov>
2018-02-15 02:55:20 -0600

    CanvasLineChart work

--- UserWebGUI/js/CanvasLineChart.js
+++ UserWebGUI/js/CanvasLineChart.js
@@ -5,7 +5,10 @@
  * 	  with modifications to support more functionality 
  * 
  * 
- * 
+ *      "0" on X-Axis should be min X Value, regardless of what that value is
+ * 	Need to allow for Negitive Y vals, have to change data rendering and increment labeling
+ * 		- most likely will have to find y vals above and below max and min y vals that are divisible by the number of hdivs cleanly, then graph and draw the grid/incr labels based on that
+ * 			- current way is close, but last (topmost) data pt is rendering wrong ( off by lower bound), and need to find a way to ensure that "0" is one of the labels
  * 
  */
 
@@ -15,22 +18,26 @@ var LineChart = function() {
   var gridCtx; //2D Context for the grid/labels
   var gridCanvas
   var dataCanvas
-  var margins = {top: 50, bottom: 50, left: 50, right: 50}; //margins
+  var margins = {top: 50, bottom: 50, left: 75, right: 50}; //margins
   var chartHeight, chartWidth; // width of the whole canvas area
   var useGrid = true;
   var useDataLabels = false;
   var useAxisLabels = true;
   var usePersistanceDisplay = false; //bool vals to select what features to use
+  var useIncrLabels = true;
   var fadeGblAlpha = 0.05; //Global Alpha value for fadeout function 
   var timeoutVal = 150; //Timeout val uses for fadeout function
   var yMaxPx, xMaxPx; // area of the chart itself, excluding axis/margin etc. 
   var data; // 
-  var maxYVal, maxXVal;//"Size" of the internal chart area
+  var maxYVal, maxXVal;//max X/Y Values of the data
+  var minXVal, minYVal;//min X/Y Values of the data
   var xInc, yInc; //Increments for data increment labels
   var xRatio, yRatio;
+  var hDivs, vDivs;
   var gridStrokeStyle = "lightgray";
   var dataStrokeStyle = "black";
   var dataLineWidth = 2;
+  var ptArray = [];
 
 var render = function(dataId,gridId,inData){
   data = inData;
@@ -44,6 +51,11 @@ var render = function(dataId,gridId,inData){
   if(data.gridStrokeStyle != null){useAxisLabels=data.gridStrokeStyle};
   if(data.dataStrokeStyle != null){dataStrokeStyle=data.dataStrokeStyle};
   if(data.dataLineWidth != null){dataLineWidth=data.dataLineWidth};
+  if(data.HorizontalDivs != null){hDivs=data.HorizontalDivs}
+    else{hDivs = data.dataPoints.length-1};
+  if(data.VerticalDivs != null){vDivs=data.VerticalDivs}
+    else{vDivs = data.dataPoints.length-1};
+  
 
   
   gridCanvas = document.getElementById(gridId);
@@ -56,19 +68,35 @@ var render = function(dataId,gridId,inData){
   yMaxPx = data.chartHeight;
   chartHeight = yMaxPx + (margins.top + margins.bottom);
   
-  maxXVal = getMaxXVal();
-  maxYVal = getMaxYVal();
+  maxXVal = getMaxXVal()//Math.round(getMaxXVal() / 10) * 10;
+  maxYVal = Math.round(getMaxYVal() / 10) * 10;
+  minXVal = getMinXVal()//Math.round(getMinXVal() / 10) * 10;
+  minYVal = Math.round(getMinYVal() / 10) * 10;
+
+  console.log(maxXVal);
   
-  yRatio = yMaxPx/maxYVal;
-  xRatio = xMaxPx/maxXVal;
+  if(minYVal < 0){
+  yRatio = yMaxPx/(maxYVal-minYVal); //set so that we can use minVal as our lower bound on the chart
+  }
+  else{
+  yRatio = yMaxPx/(maxYVal); //set so that we can use 0 as our lower bound on the chart
+  minYVal = 0;
+  }
+  
+  xRatio = xMaxPx/(maxXVal-minXVal); //min X val is always going to be our leftmost value, so no need to check here
+  
+  
+  console.log(yRatio)
   
   dataCanvas.style.height = dataCanvas.height = chartHeight*window.devicePixelRatio;
   dataCanvas.style.width = dataCanvas.width = chartWidth*window.devicePixelRatio;
-  dataCtx.setTransform(window.devicePixelRatio,0,0,window.devicePixelRatio,0,0);
+  dataCtx.scale(window.devicePixelRatio,window.devicePixelRatio);
+  
   
   gridCanvas.style.height = gridCanvas.height = chartHeight*window.devicePixelRatio;
   gridCanvas.style.width = gridCanvas.width = chartWidth*window.devicePixelRatio;
-  gridCtx.setTransform(window.devicePixelRatio,0,0,window.devicePixelRatio,0,0);
+  gridCtx.scale(window.devicePixelRatio,window.devicePixelRatio);  
+ 
   
   //console.log(window.devicePixelRatio);
   
@@ -78,29 +106,23 @@ var render = function(dataId,gridId,inData){
 
 var renderChart = function () { //Render the elements of the chart that use selects along with the data
   //render things if they're set to be used 
-  if (data.useGrid == true){renderGrid()}; 
-  if (data.useDataLabels == true){renderDataLabels()}; //Data labels on the points themself
-  if (data.useAxisLabels == true){renderAxisLabels()}; //Axis titles/labels
+  if (useGrid == true){renderGrid()}; 
+  if (useDataLabels == true){renderDataLabels()}; //Data labels on the points themself
+  if (useAxisLabels == true){renderAxisLabels()}; //Axis titles/labels
+  if (useIncrLabels == true){renderIncrLabels()}; //Labels on the increments of the grid
   
   renderData(data.dataPoints);
-  /*
-  if (data.renderTypes == undefined || data.renderTypes == null) {data.renderTypes = [renderType.lines]}
-  for (var i = 0; i < data.renderTypes.length; i++){
-      renderData(data.renderTypes[i]);
-  }
-  */
 };
 
 var renderGrid = function () {
   //draw the grid
-  hDivs = data.HorizontalDivs;
-  vDivs = data.VeticalDivs;
 
   var vDivSpacing = (xMaxPx/vDivs); 
   var hDivSpacing = (yMaxPx/hDivs);
 
-    gridCtx.beginPath();
-    gridCtx.lineWidth=1;
+  gridCtx.beginPath();
+  gridCtx.lineWidth=1;
+    
   for (var x =margins.left; x <= xMaxPx + margins.left; x += vDivSpacing){ //draw vertical lines
     gridCtx.moveTo(0.5 + x, margins.top);
     gridCtx.lineTo(0.5 + x, yMaxPx+margins.top);
@@ -118,6 +140,7 @@ var renderGrid = function () {
 
 
 var renderDataLabels = function () {
+  
   return;
 };
 
@@ -129,14 +152,15 @@ var renderAxisLabels = function () {
   //Render the Title
   if(data.title != "" && data.title != null){
   var size = gridCtx.measureText(data.title);
-  gridCtx.fillText(data.title, (chartWidth/2), (margins.top/1.5));
+  gridCtx.fillText(data.title, (margins.left+(xMaxPx/2)), (margins.top/1.5));
   }
+
   
   //X-Axis Label
   if(data.xLabel != "" && data.xLabel != null){
-  console.log(data.xLabel);
+  //console.log(data.xLabel);
   size = gridCtx.measureText(data.xLabel);
-  gridCtx.fillText(data.xLabel, (chartWidth/2), (yMaxPx+margins.top+margins.bottom/1.5))//, margins.left + (xMaxPx/2)-(size.width/2), yMaxPx + (margins.bottom/1.5));
+  gridCtx.fillText(data.xLabel, (margins.left+(xMaxPx/2)), (yMaxPx+margins.top+margins.bottom/1.5))//, margins.left + (xMaxPx/2)-(size.width/2), yMaxPx + (margins.bottom/1.5));
   }
   
   //Y-Axis Label - save the canvas, rotate it to render text for the Y Axis label, then resore it once text rendered
@@ -144,11 +168,66 @@ var renderAxisLabels = function () {
   gridCtx.save();
   gridCtx.rotate(-Math.PI / 2);
   gridCtx.font = labelFont;
-  gridCtx.fillText(data.yLabel, (yMaxPx / 2) * -1, margins.left / 1.5);
+  gridCtx.fillText(data.yLabel, (margins.top+(yMaxPx/ 2)) * -1, margins.left / 4);
   gridCtx.restore();  
   }
   
 }
+
+var renderIncrLabels = function() {
+
+  gridCtx.font = (data.dataPointFont != null) ? data.dataPointFont : '10pt Calibri';
+  
+  if (minYVal < 0){
+    startYLabel = minYVal;
+  }
+  else{
+    startYLabel = 0;
+  }
+  
+  
+  var startXLabel = minXVal;
+  
+  console.log("First X Label: " + startXLabel);
+  console.log("First Y Label: " + startYLabel);
+  
+  var xPos = 0;
+  var yPos = 0;
+  var vDivSpacing = (xMaxPx/vDivs); 
+  var hDivSpacing = (yMaxPx/hDivs);
+  
+  gridCtx.beginPath();
+  gridCtx.lineWidth=1;
+  
+  
+  
+
+  for (var i = 0; i < hDivs+1; i++){
+      yPos += (i == 0) ? margins.top : hDivSpacing; 
+      var txt = maxYVal-((i == 0) ? Math.abs(startYLabel) : i*((maxYVal-minYVal)/hDivs));
+      txt = +txt.toFixed(1);
+      console.log(i + " " + txt);
+      var txtSize = gridCtx.measureText(txt);
+      gridCtx.fillText(txt, margins.left - ((txtSize.width >= 14) ? txtSize.width : 10) -7, yPos + 4);
+    
+  }
+  
+  for (var i = 0; i < vDivs+1; i++){
+      xPos += (i == 0) ? margins.left : vDivSpacing; 
+      var txt =(i*((maxXVal-minXVal)/vDivs))+startXLabel;
+      txt = +txt.toFixed(1);
+      var txtSize = gridCtx.measureText(txt);
+      gridCtx.fillText(txt, xPos, margins.top + yMaxPx + (margins.bottom / 3));    
+  }
+  
+  
+  gridCtx.strokeStyle = gridStrokeStyle;
+  gridCtx.stroke();
+  gridCtx.closePath();  
+  
+}
+
+
 
 var renderData = function (dataPts) {
   var lastPt = {x: 0, y: 0};
@@ -156,17 +235,16 @@ var renderData = function (dataPts) {
 
     
   dataCtx.beginPath(); 
-  for(let pt of dataPts){
-   
-    thisPt.x = ((pt.x) * xRatio) + margins.left;
-    thisPt.y = ((maxYVal - pt.y) * yRatio) + margins.top;
-    
-    
+  for(var i = 0; i < data.dataPoints.length; i++){
+    var pt = data.dataPoints[i];
+    thisPt.x = ((pt.x-data.dataPoints[0].x) * xRatio) + margins.left;//treat inital x value as "0", subtract init x from current to do so
+    thisPt.y = ((maxYVal - pt.y) * yRatio) + margins.top; //reversing because canvas coords are weird and start in the top left
+    console.log("Current Point coords - x: " + thisPt.x + " y: " + thisPt.y)
    if (!(lastPt.x == 0) && !(lastPt.y == 0)){
-
      dataCtx.moveTo(lastPt.x+0.5,lastPt.y+0.5);
      dataCtx.lineTo(thisPt.x+0.5,thisPt.y+0.5);
     }
+    ptArray.push(thisPt);
     lastPt.x = thisPt.x;
     lastPt.y = thisPt.y;
     
@@ -234,6 +312,29 @@ var getMaxYVal = function () {
   } 
   return maxVal
 }  
+
+
+var getMinXVal = function () {
+  var minVal = data.dataPoints[0].x;
+  for (let pt of data.dataPoints){
+   if (pt.x < minVal){
+     console.log("X: " + pt.x + " < " + minVal + ", setting minVal to that.")
+     minVal = pt.x     
+    }
+  } 
+  return minVal
+}
+  
+var getMinYVal = function () {
+  var minVal = data.dataPoints[0].y;
+  for (let pt of data.dataPoints){
+    if (pt.y < minVal){
+     minVal = pt.y
+    }
+  } 
+  return minVal
+}  
+
   
   
   return{
